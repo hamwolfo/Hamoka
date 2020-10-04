7875356bb7f074f67b253759ebe8affb7523d395
hamwolfo <71834951+hamwolfo@users.noreply.github.com>
2020-10-04 19:02:15 +0300

    Update choice.py

--- choice.py
+++ choice.py
@@ -1,11 +1,4 @@
-# Decompiled By RandiSr
-# Github : https://github.com/RANDIOLOY
-# uncompyle6 version 3.7.4
-# Python bytecode 2.7
-# Decompiled from: Python 2.7.18 (default, Jul  8 2020, 22:53:57) 
-# [GCC 4.2.1 Compatible Android (5220042 based on r346389c) Clang 8.0.7 (https://
-# Embedded file name: dg
-try:
+
     import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass, mechanize, requests
     from multiprocessing.pool import ThreadPool
     from requests.exceptions import ConnectionError
